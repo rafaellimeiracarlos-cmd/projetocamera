271b69eebc99fd9b2b06f81a3a654acea64bcf16
rafael <rafaellimeiracarlos@gmail.com>
2026-05-28 21:18:13 -0300

    feat: funcionalidade da camera

---
+++ src/js/script.js
@@ -0,0 +1,60 @@
+//declaracoes dos elementos
+
+const videoElemento = document.getElementById("video");
+const botaoScanear = document.getElementById("btn-texto");
+const resultado = document.getElementById("saida");
+const canvas = document.getElementById("canvas");
+
+
+// Método Habilitar Câmera
+
+async function configurarCamera(){
+    try{
+        const midia = await navigator.mediaDevices.getUserMedia({
+            video: {facingMode: "environment"},
+            audio: false
+        })
+
+
+    }catch(erro){
+        resultado.innerText="Erro ao acessar a câmera",erro;
+    }
+}
+configurarCamera();
+
+botaoScanear; onclick = async ()=>{
+
+botaoScanear.disabled = true;
+resultado.innerText = "Fazendo a leitura.... aguarde";
+// preparar o cenario em 2d do canvas
+
+const contexto = canvas.getContext("2d")
+
+//ajustar tamanho 
+
+canvas.width = videoElemento.videowidth;
+
+canvas.height = videoElemento.videoheight;
+
+//limpa e garante que a orientacao seja a padrao ( nao espelha )
+
+contexto.setTransform(1,0,0,1,0,0);
+contexto.filter= "contrast(1,2) grayscale(1)";
+contexto.drawImage(videoElemento,0,0, canvas.width,canvas.height);
+
+
+    try{
+        const {data: {text}} = await Tesseract.recognize(
+            canvas,
+            'por' // idioma do texto
+        );
+        const textoFinal = text.trim();
+        resultado.innerText = textoFinal.length > 0 ? textoFinal : "Não possivel capturar o texto";
+    }catch(erro){
+        console.error(erro);
+        resultado.innerText = "Erro no Processamento", erro;
+    } finally{
+        botaoScanear.disabled= false;
+    }
+}
+
